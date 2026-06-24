b9c8644ffc92e2fd470ead5896c0a5ecc7495c34
Pasindu20020817 <chethiyawijesingha17@gmail.com>
2026-06-24 14:07:13 +0530

    Setup authentication foundation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+//////////////////////////////////////////////////////////////
+//temporerly for testing prisma connection
+const prisma = require("./src/config/prisma");
+
+// Test database connection
+async function testDB() {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed");
+    console.error(error);
+  }
+}
+testDB();
+///////////////////////////////////////////////////////////////
+
 const app = express();
 
 app.use(cors());
@@ -15,3 +32,4 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
