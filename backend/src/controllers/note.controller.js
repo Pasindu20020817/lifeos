adbba230853a9795db4b00d922c98fa643369fde
Pasindu20020817 <chethiyawijesingha17@gmail.com>
2026-07-29 12:18:38 +0530

    feat: implement note read endpoints

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -47,6 +47,88 @@ const createNote = async (req, res) => {
 
 };
 
+/**
+ * Get All Notes
+ *
+ * Returns only notes belonging to
+ * the authenticated user.
+ */
+const getAllNotes = async (req, res) => {
+  try {
+
+    const notes = await prisma.note.findMany({
+
+      where: {
+        userId: req.user.userId,
+      },
+
+      orderBy: {
+        createdAt: "desc",
+      },
+
+    });
+
+    return res.status(200).json({
+      success: true,
+      count: notes.length,
+      notes,
+    });
+
+  } catch (error) {
+
+    console.error("Get Notes Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+
+  }
+};
+
+/**
+ * Get Single Note By ID
+ */
+const getNoteById = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    const note = await prisma.note.findFirst({
+
+      where: {
+        id,
+        userId: req.user.userId,
+      },
+
+    });
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      note,
+    });
+
+  } catch (error) {
+
+    console.error("Get Note By ID Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+
+  }
+};
+
 module.exports = {
   createNote,
+  getAllNotes,
+  getNoteById,
 };
--- backend/src/routes/note.routes.js
+++ backend/src/routes/note.routes.js
@@ -2,28 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const authenticate =
-  require("../middleware/auth.middleware");
+const authenticate = require("../middleware/auth.middleware");
+const validate = require("../middleware/validate.middleware");
+const {createNoteSchema, } = require("../validations/note.validation");
+const {createNote, getAllNotes, getNoteById, } = require("../controllers/note.controller");
 
-const validate =
-  require("../middleware/validate.middleware");
-
-const {
-  createNoteSchema,
-} = require("../validations/note.validation");
-
-const {
-  createNote,
-} = require("../controllers/note.controller");
-
-/**
- * Create Note
- */
-router.post(
-  "/",
-  authenticate,
-  validate(createNoteSchema),
-  createNote
-);
+//Create Note
+router.post("/", authenticate, validate(createNoteSchema), createNote );
+//Get All Notes
+router.get("/", authenticate, getAllNotes );
+//Get Note By ID
+router.get("/:id", authenticate, getNoteById );
 
 module.exports = router;
